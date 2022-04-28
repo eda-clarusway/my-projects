9451aaf98a8c8ad43b11f861de306d4a47daf9a0
eda <101637060+eda-clarusway@users.noreply.github.com>
2022-04-28 16:15:43 +0300

    added

---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World from Flask!!!"
+
+@app.route('/second')
+def second(): 
+    return 'Bize Her Yer DevOps!!!'
+
+@app.route('/third/subthird')
+def third():
+    return 'this is the subpage of third page'
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id number of this page is {id}'   
+
+
+
+if __name__ == '__main__' :
+    app.run(debug=True, port=5000)    
